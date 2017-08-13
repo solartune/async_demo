48ce3ad34edb3c04752d7a149c3b24492133e0bc
solartune <solartune@rambler.ru>
2017-08-13 18:11:35 +0300

    added TreeView with routers

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,11 @@
-from .views import index
+from .views import TreeView
+from aiohttp import web
 
 
 def setup_routes(app):
-    app.router.add_get('/', index)
+    # handler = TreeView
+    # app.router.add_get('/', handler.get)
+    # app.router.add_post('/', handler.post)
+    # app.router.add_get('/detail/{obj_id:\d+}', handler.get_object)
+    # app.router.add_post('/search', handler.search)
+    app.router.add_route('*', '/', TreeView)
--- application/views.py
+++ application/views.py
@@ -1,5 +1,41 @@
-from aiohttp import web
+import logging
+import json
+import os
+
+from aiohttp.web import View, json_response, Response
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import ReturnDocument
+
+logger = logging.getLogger(__file__)
 
 
-async def index(request):
-    return web.Response(text='Hello Aiohttp!')
+class TreeView(View):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_client = AsyncIOMotorClient(
+            os.getenv('MONGODB_HOST'), int(os.getenv('MONGODB_PORT')))
+        self.db = self.db_client.asyncio_db
+
+    async def get(self):
+        objects = self.db.tree.find()
+        return json_response(await objects.to_list(None))
+
+    async def post(self):
+        data = await self.request.json()
+        obj = await self.db.tree.find_one_and_update(
+            {'_id': data['id']},
+            {'$set': {'text': data['text']}},
+            projection={'text': True, '_id': False},
+            upsert=True,
+            return_document=ReturnDocument.AFTER)
+        return json_response({'obj': str(obj)})
+
+    async def get_object(self):
+        obj_id = self.match_info.get('obj_id')
+        data = {'type': 'get_object', 'obj_id': obj_id}
+        return json_response(data)
+
+    async def search(self):
+        data = {'type': 'search'}
+        return json_response(data)
